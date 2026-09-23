8ca67efa7657680e685b138e76f3e22d4318881a
Diego Gaizka Riquelme Anguita <dg.riquelme@duocuc.cl>
2026-09-23 18:35:05 -0300

    fix perfiles.py eliminar líneas sobrantes

--- perfiles.py
+++ perfiles.py
@@ -19,16 +19,14 @@ miembros = [
         "habilidad": "Comer completos",
         "lenguaje": "Python" 
     },
-]   {
+    {
         "nombre": "Gaizka Riquelme",
         "rol": "Líder de ciberseguridad",
         "habilidad": "Hackear los macdonals",
         "lenguaje": "Python"
         
         
-    },
-Romper producción justo antes de una presentación"
-    },
+    }
     # ---------------------------------------------------
     # ESTUDIANTES: Agreguen sus diccionarios aquí abajo.
     # No olviden poner una coma al final de cada diccionario.
