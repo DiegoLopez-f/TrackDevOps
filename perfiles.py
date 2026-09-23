8b76ef90a34568441a39f1404eb66c1c4aadb339
gaxz <jo.oyarzunt@duocuc.cl>
2026-09-23 13:55:01 -0300

    Update perfiles.py

--- perfiles.py
+++ perfiles.py
@@ -1,11 +1,26 @@
 # perfiles.py
 # Instrucciones: ¡Agrega tu perfil a la lista usando un diccionario!
-
 miembros = [
     {
         "nombre": "Diego López",
         "rol": "Líder Track DevOps",
-        "habilidad": "Romper producción justo antes de una presentación"
+        "habilidad": "Romper producción justo antes de una presentación",
+        "lenguaje": "Python"
+    },
+    {
+        "nombre": "Joaquín Oyarzún",
+        "rol": "Jefe de las Gallinas",
+        "habilidad": "Invocar gallinas",
+        "lenguaje": "Python"
+    },
+    {
+        "nombre": "Mattias",
+        "rol": "Líder de Metaverso",
+        "habilidad": "Comer completos",
+        "lenguaje": "Python"
+    },
+]
+Romper producción justo antes de una presentación"
     },
     # ---------------------------------------------------
     # ESTUDIANTES: Agreguen sus diccionarios aquí abajo.
